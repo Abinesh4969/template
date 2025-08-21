f7486a2038d93ba78d8c2c1e991bc6e3e57fda2b
abinesh4969 <abineshnatrajan@gmail.com>
2025-08-21 15:46:52 +0530

    add api.php

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+         return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'roles' => $this->getRoleNames()->first(), 
+            // 'otp_verified'=> $this->otp_verified,
+            // 'kyc_verified'=> $this->kyc_verified,
+        ];
+    }
+}
